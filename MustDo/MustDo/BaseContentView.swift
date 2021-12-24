a5a63f14c30cf27a6013ff8c52c15f737e397b77
BeomKey <nkb5813@naver.com>
2021-12-24 15:36:33 +0900

    의존성 주입 및 List 생성

--- MustDo/MustDo/BaseContentView.swift
+++ MustDo/MustDo/BaseContentView.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 
 struct BaseContentView: View {
+    @ObservedObject var viewModel: BaseContentViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(viewModel.listDataSource) { dataSource in
+            Text("\(dataSource.description)")
+        }
     }
 }
 
 struct BaseContentView_Previews: PreviewProvider {
     static var previews: some View {
-        BaseContentView()
+        let viewModel = BaseContentViewModel()
+        BaseContentView(viewModel: viewModel)
     }
 }
